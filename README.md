bf763d23f704e0833f274887a7a51b230787aac6
Sergey Galkin <srggal@gmail.com>
2016-10-03 16:51:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Simple Riak Spark Java Connector Example
+
+It's a small example application how to use [Riak Spark Connector](https://github.com/basho/spark-riak-connector) from a Java code, it calculates the number of values in the bucket.
+
+## Run the example
+
+You need to have Riak and Spark clusters up and running.
+
+To spin-up Spark Cluster [docker-spark](https://github.com/gettyimages/docker-spark#docker-compose-example) might be used, it is only required to change spark image to 1.6.2-hadoop-2.6:
+
+```
+sed -i -e 's/gettyimages\/spark/gettyimages\/spark\:1.6.2-hadoop-2.6/g' ./docker-compose.yml
+```
+
+** TODO NEED TO ADD how to run riak from docker **
+
+```
+mvn package exec:java -Dspark.master=<spark-master-url> -Dspark.riak.connection.host=<riak-host-and-port>
+```
+ 
+Example:
+```
+mvn package exec:java -Dkvbucket=test -Dspark.master=spark://172.17.0.2:7077 -Dspark.riak.connection.host=192.168.2.236:10017
+```
+
+
+As a result of the execution you should see in the output something like that:
+
+```
+16/10/03 15:37:55 INFO TaskSchedulerImpl: Removed TaskSet 0.0, whose tasks have all completed, from pool 
+16/10/03 15:37:55 INFO DAGScheduler: Job 0 finished: count at App.java:28, took 6.450440 s
+
+
+
+COUNT: 0
+
+
+16/10/03 15:37:55 INFO SparkUI: Stopped Spark web UI at http://192.168.99.1:4040
+16/10/03 15:37:55 INFO SparkDeploySchedulerBackend: Shutting down all executors
+```
+  
+whereas COUNT: 0 means that there are no values in the bucket 'test'.
+
+You could use following commands: add and check data to/in bucket:
+
+- put value to the bucket:
+    ```
+    curl -X POST -H "Content-Type: plain/text" --data "Test1"  localhost:10018/buckets/test/keys/k1
+    ```
+- list all teh keys in the bucket:
+    ```
+    curl localhost:10018/buckets/test/keys?keys=true
+    ```
+ 
+
+ 
---
+++ src/main/java/com/github/srgg/App.java
@@ -0,0 +1,61 @@
+package com.github.srgg;
+
+import com.basho.riak.client.core.query.Namespace;
+import com.basho.riak.spark.japi.SparkJavaUtil;
+import com.basho.riak.spark.japi.rdd.RiakJavaRDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String bucket = System.getenv("kvbucket");
+        if (bucket == null) {
+            bucket = "test";
+        }
+
+        final SparkConf conf = new SparkConf().setAppName("Java Riak Spark Connector usage example");
+        conf.setIfMissing("spark.master", "spark://172.17.0.2:7077");
+        conf.setIfMissing("spark.riak.connection.host", "192.168.2.236:10017");
+
+        final String depsPath = getDepsDir();
+
+        conf.setJars(new String[]{depsPath + "/spark-riak-connector_2.10-1.6.0-uber.jar"});
+
+        final JavaSparkContext jsc = new JavaSparkContext(conf);
+        try {
+            final RiakJavaRDD<String> rdd = SparkJavaUtil.javaFunctions(jsc).riakBucket(new Namespace(bucket), String.class).queryAll();
+            final long r = rdd.count();
+
+            System.out.println("\n\n\nCOUNT: " + r + "\n\n");
+        } finally {
+            jsc.stop();
+        }
+    }
+
+    private static String getDepsDir() {
+        final java.io.InputStream is = App.class.getResourceAsStream("/app.properties");
+
+        if (is == null) {
+            throw new IllegalStateException("Can't load 'app.properties'.");
+        }
+
+        final Properties props = new Properties();
+        try {
+            props.load(is);
+            final String s = props.getProperty("dependencies-dir");
+
+            if (s == null) {
+                throw new IllegalStateException("The 'dependencies-dir' is not defined in app.properties.");
+            }
+
+            return s;
+        } catch (IOException e) {
+            throw new RuntimeException("Error occured during app.properties load.");
+        }
+    }
+}
